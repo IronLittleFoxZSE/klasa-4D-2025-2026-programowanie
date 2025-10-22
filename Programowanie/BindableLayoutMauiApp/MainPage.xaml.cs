9a2cadafe616403f335f0c4159bb527da37c3bd9
f <f@f.pl>
2025-10-22 12:50:01 +0200

    koniec omawiania

---
+++ Programowanie/BindableLayoutMauiApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using BindableLayoutMauiApp.Models;
+using System.Collections.ObjectModel;
+
+namespace BindableLayoutMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        /*private List<TodoTask> todoTasks;
+        public List<TodoTask> TodoTasks
+        {
+            get { return todoTasks; }
+            set { todoTasks = value; OnPropertyChanged(); }
+        }*/
+
+        public ObservableCollection<TodoTask> TodoTasks { get; set; }
+
+        public string NewTitle { get; set; }
+        public MainPage()
+        {
+            TodoTasks = new ObservableCollection<TodoTask>()
+            {
+                new TodoTask(){Title = "Zadanie pierwsze", IsCompleted = false, Column=1, Row=1},
+                new TodoTask(){Title = "Zadanie drugie", IsCompleted = false},
+                new TodoTask(){Title = "Zadanie trzecie", IsCompleted = true},
+                new TodoTask(){Title = "Zadanie czwarte", IsCompleted = false},
+            };
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //TodoTasks = new List<TodoTask>();
+            TodoTasks.Add(new TodoTask() { Title = NewTitle, IsCompleted = false });
+        }
+    }
+}
---
+++ Programowanie/BindableLayoutMauiApp/Models/TodoTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindableLayoutMauiApp.Models
+{
+    public class TodoTask : BindableObject
+    {
+        public string Title { get; set; }
+
+        private bool isCompleted;
+        public bool IsCompleted
+        {
+            get { return isCompleted; }
+            set { isCompleted = value; OnPropertyChanged(); }
+        }
+
+        public int Column { get; set; }
+        public int Row { get; set; }
+    }
+}
